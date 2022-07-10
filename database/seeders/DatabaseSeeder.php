3e9c9ae28e455a0574b8b47a3c3cf7227a0f0e5b
abdesselam-arch <abdesselam-arch>
2022-07-10 16:05:54 +0100

    Fourth commit 1605

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Continent;
+use App\Models\Country;
+use App\Models\Product;
+use Database\Factories\CountryFactory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,10 +19,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $continents = [
+            ['id' => 1, 'name' => 'Europe',],
+            ['id' => 2, 'name' => 'Asia',],
+            ['id' => 3, 'name' => 'Africa',],
+            ['id' => 4, 'name' => 'South America',],
+            ['id' => 5, 'name' => 'North America',],
+        ];
+        foreach ($continents as $continent) {
+            \App\Models\Continent::factory()->create($continent)
+                ->each(function ($c) {
+                    $c->countries()->saveMany(Country::factory(10)->make());
+                });;
+        }
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        //Product::factory(100)->create();
     }
 }
